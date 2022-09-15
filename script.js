9ffbd5b2c5d7f4a8fc5450ea32e3e66e9d5819bc
kanizfatima2 <kaniz686928@gmail.com>
2022-09-15 14:49:30 +0600

    s

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ fetch("./texts.json")
 // checks the user typed character and displays accordingly
 const typeController = (e) => {
   const newLetter = e.key;
-  console.log('kaniz')
 
   // Handle backspace press
   if (newLetter == "Backspace") {
@@ -46,7 +45,8 @@ const typeController = (e) => {
   if (newLetterCorrect) {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
-    display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`
+    errorCount++;
   }
 
   // check if given question text is equal to user typed text
